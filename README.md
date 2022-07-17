efdea1ccaa9e91b7c4d70ac2becb0a1d385e52a8
mulfranck <mulfranck5@gmail.com>
2022-07-17 19:00:34 +0100

    refact: use data from a json server

--- README.md
+++ README.md
@@ -1,3 +1,37 @@
 # React multra Blog
 
 Learning react from _Net Ninja_ tutorial project Dojo Blog.
+
+
+### Learnt
+#### 1. ```useEffect``` hook
+
+Used to fire a function each time there is a rerender
+
+```jsx
+useEffect( () => {
+  // function body
+})
+```
+##### dependency array
+```jsx
+useEffect( () => {
+  // function body
+}, [])
+```
+* Fires the function once, when the page renders the first time.
+* A good place to fetch data
+
+```jsx
+const [name, setName] = useState('Mario');
+...
+useEffect( () => {
+  // function body
+}, [name])
+```
+`useEffect()` when watch the state name `name` if there is a change on this state the useEffect function is fired.
+Thus, this would be fired when the page first renders, and whenever the `name` state changes.
+
+#### 2. json-server
+```npm -i --location=global json-server```
+```npm json-server --watch data/db.json --port 9000```
--- src/Home.js
+++ src/Home.js
@@ -1,24 +1,24 @@
+import { useEffect, useState} from "react";
 import Bloglist from "./Bloglist";
 
-const { useState } = require("react")
-
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    {title: "Wsl crash on purge zsh", body:"", author:"mulfranck", id:1},
-    {title: "The direction toward ubuntu in windows", body:"", author:"Nigga Joe", id:2},
-    {title: 'How to safely uninstall "Oh my zsh"', body:"", author:"mulfranck", id:3},
-  ])
+  const [blogs, setBlogs] = useState(null)
   
   const handleDelete = (id) => {
     const newBlogs = blogs.filter(blog => (blog.id !== id))
     setBlogs(newBlogs)
-    return 1
   }
+
+  useEffect(() => {
+    fetch('http://localhost:8000/blogs')
+      .then((resp) => resp.json())
+      .then(data => setBlogs(data))
+  }, [])
   
   return (
     <div className="home">
-      <Bloglist blogs={blogs} title={"All blogs"} handleDelete={handleDelete}/>
-      <Bloglist blogs={blogs.filter(blog => blog.author === 'mulfranck')} title={"@mulfranck's blogs"}/>
+      {blogs && <Bloglist blogs={blogs} title={"All blogs"} handleDelete={handleDelete}/>}
+      {blogs && <Bloglist blogs={blogs.filter(blog => blog.author === 'mulfranck')} title={"@mulfranck's blogs"}/>}
     </div>
   )
 }
