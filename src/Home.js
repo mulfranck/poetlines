7132fdee7dc947f7d424ce19401b468768245c14
mulfranck <mulfranck5@gmail.com>
2022-07-17 20:24:27 +0100

    feat: add fetch error handling

--- src/Home.js
+++ src/Home.js
@@ -2,7 +2,9 @@ import { useEffect, useState} from "react";
 import Bloglist from "./Bloglist";
 
 const Home = () => {
+  const url = 'http://localhost:8000/blogs';
   const [blogs, setBlogs] = useState(null);
+  const [error, setError] = useState(null);
   const [isPending, setIsPending] = useState(true);
   
   const handleDelete = (id) => {
@@ -11,19 +13,26 @@ const Home = () => {
   }
 
   useEffect(() => {
-    fetch('http://localhost:8000/blogs')
-      .then((resp) => resp.json())
-      .then(data => {
+    fetch(url)
+      .then((resp) => {
+        if (!resp.ok) {
+          setIsPending(false);
+          throw Error(`Could not fetch data from ${url}`);
+        }
+        return resp.json();
+      }).then(data => {
         setBlogs(data);
         setIsPending(false);
-      });
+      }).catch(e => {
+        setError(e.message)
+      })
   }, [])
   
   return (
     <div className="home">
+      {error && <div className="loading"><strong>{error}</strong></div>}
       {isPending && <div className="loading"><strong>Loading...</strong></div>}
       {blogs && <Bloglist blogs={blogs} title={"All blogs"} handleDelete={handleDelete}/>}
-      {blogs && <Bloglist blogs={blogs.filter(blog => blog.author === 'mulfranck')} title={"@mulfranck's blogs"}/>}
     </div>
   )
 }
