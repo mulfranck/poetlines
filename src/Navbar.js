5a82209116c138c847d3a8938b56fec4b438319a
mulfranck <mulfranck5@gmail.com>
2022-08-14 21:58:59 +0100

    add navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,16 +1,40 @@
 const Navbar = () => {
-
-
     return (
-        <header className="header">
-            <h1>multra</h1>
+        <header className="navbar navbar-expand-md navbar-dark bg-dark">
+            <nav className="container">
+                <a className="navbar-brand" href="#">Poetline</a>
 
-            <nav className="nav-bar">
-                <ul>
-                    <li className="nav-list"><a href='/' className="nav-link">Home</a></li>
-                    <li className="nav-list"><a href='/new' className="nav-link">New blog</a></li>
-                    <li className="nav-list"><a href='/search' className="nav-link">search</a></li>
-                </ul>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapseContent" aria-controls="navbarCollapseContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                <div className="collapse navbar-collapse" id="navbarCollapseContent">
+                    <form className="d-flex mx-auto" role="search">
+                        <div className="input-group">
+                            <input className="form-control" type="search" placeholder="Search" aria-label="Search" />
+                            <button className="btn btn-outline-success" type="submit">Search</button>
+                        </div>
+                    </form>
+                    <ul className="navbar-nav py-2">
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li className="nav-item dropdown">
+                            <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Forms
+                            </a>
+                            <ul className="dropdown-menu">
+                                <li><a className="dropdown-item" href="#">Action</a></li>
+                                <li><a className="dropdown-item" href="#">Another action</a></li>
+                                <li><hr className="dropdown-divider" /></li>
+                                <li><a className="dropdown-item" href="#">Something else here</a></li>
+                            </ul>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#"> Log in </a>
+                        </li>
+                    </ul>
+                </div>
             </nav>
         </header>
     )
