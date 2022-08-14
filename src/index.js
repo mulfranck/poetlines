1878b826a1c976cb8f939fa9c00284e8a4bd1990
mulfranck <mulfranck5@gmail.com>
2022-08-14 22:10:10 +0100

    style: add bootstrap style, script and icons

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'bootstrap/dist/js/bootstrap.bundle'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
-import React from 'react';
 import ReactDOM from 'react-dom/client';
+import React from 'react';
 import App from './App';
 
 
